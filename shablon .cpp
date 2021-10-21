a273d1aa95dd919cbf63b86a3932690c5129af2e
TamaraAV <92917483+TamaraAV@users.noreply.github.com>
2021-10-21 16:52:38 +0300

    Create shablon .cpp

---
+++ shablon .cpp
@@ -0,0 +1,24 @@
+#include "Shablon.h"
+int Crypt(const char* arr1, const char* arr2, char* input, char* output)
+{
+	int i;
+	while (*input)
+	{
+		for (i = 0; i < strlen(arr1); i++)
+		{
+			if (*input == arr1[i])
+			{
+				*output = arr2[i];
+				break;
+			}
+		}
+		if (i == strlen(arr1))
+		{
+			*output = *input;
+		}
+		output++;
+		input++;
+	}
+	*output = 0;
+		return 0;
+}
